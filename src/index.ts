91573eecd5d9aa74dfab0e4a7f790d85391427cc
Sourabh Singh <imhashvoid8@gmail.com>
2025-02-27 20:01:52 +0530

    Add backend

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import { userRoutes } from "./routes/user";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import { userContent } from "./routes/content";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api/v1/user/", userRoutes);
+app.use("/api/v1/content/", userContent);
+
+
+app.listen(process.env.PORT, async () => {
+    await mongoose.connect(process.env.MONGOOSE_URL as string);
+    console.log(`Connected the database ${process.env.PORT}`);
+})
+
+
+
---
+++ src/routes/content.ts
@@ -0,0 +1,105 @@
+import express, { Request, Response } from "express"
+import { content } from "../schema/db";
+import { userMiddleware } from "../middleware/userMiddleware";
+import z from "zod";
+
+export const userContent = express.Router();
+
+userContent.get("/", userMiddleware, async (req: Request, res: Response) => {
+    //@ts-ignore
+    const userId = req.userId;
+    try {
+        const findContent = await content.find({
+            userId : userId
+        }).populate({
+            path : "userId"
+        })
+
+        console.log(findContent)
+
+        res.status(200).json({
+            contents: findContent
+        })
+    } catch (e) {
+        res.status(500).json({
+            msg: "Server Side Issued"
+        })
+    }
+})
+
+userContent.post("/", userMiddleware, async (req: Request, res: Response) => {
+
+    const inputSchema = z.object({
+        type: z.string(),
+        link: z.string(),
+        title: z.string().optional(),
+        tags: z.array(z.string()).optional()
+    })
+
+    const checkSchema = inputSchema.safeParse(req.body);
+
+    if (!checkSchema.success) {
+        res.status(403).json({
+            msg: checkSchema.error
+        })
+        return;
+    }
+
+    try {
+        //@ts-ignore
+        const userId = req.userId;
+        console.log(userId);
+        const fillContent = await content.create({
+            type: req.body.type,
+            link: req.body.link,
+            title: req.body.title,
+            tags: req.body.tags,
+            userId : userId
+        })
+        res.status(201).json({
+            msg: "Successfully Added Content"
+        })
+
+
+    } catch (e) {
+        res.status(500).json({
+            msg: `Server Side Error ${e}`
+        })
+    }
+
+})
+
+userContent.post("/brain/:shareLink", userMiddleware, async (req: Request, res: Response) => {
+    
+
+})
+
+userContent.delete("/:contentId", userMiddleware, async (req: Request, res: Response) => {
+    const contentId = req.params.contentId
+
+    try{
+        const findContent = await content.findOne({
+            _id : contentId
+        })
+
+        if(!findContent){
+            res.status(404).json({
+                msg : "Cannot address the content"
+            })
+            return ;
+        }
+
+        const deleteContent = await content.deleteOne({
+            _id : contentId
+        })
+
+        res.status(200).json({
+            msg : "Deleted the content"
+        })
+
+    }catch(e){
+        res.status(500).json({
+            msg : "Server side error"
+        })
+    }
+})
